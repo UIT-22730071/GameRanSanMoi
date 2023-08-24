21d7dce24913744e054184a9fba429710f21449f
UIT-22730071 <22730071@ms.uit.edu.vn>
2023-08-24 20:49:12 +0700

    HoanThienGameConRan

--- main.cpp
+++ main.cpp
@@ -12,81 +12,80 @@ void gotoxy( int column, int line );
 struct Point{
     int x,y;
 };
-
-class CONRAN {
+class CONRAN{
 public:
     struct Point A[100];
     int DoDai;
-    CONRAN() {
+    CONRAN(){
         DoDai = 3;
         A[0].x = 10; A[0].y = 10;
         A[1].x = 11; A[1].y = 10;
         A[2].x = 12; A[2].y = 10;
     }
-    void Ve(Point Qua) {
+    void Ve(Point Qua){
         for (int i = 0; i < DoDai; i++){
             gotoxy(A[i].x,A[i].y);
             cout<<"X";
         }
         gotoxy(Qua.x, Qua.y); cout<<"*";
     }
-
-    void DiChuyen() {
+    void DiChuyen(int Huong, Point& Qua){
         for (int i = DoDai-1; i>0;i--)
             A[i] = A[i-1];
-        if (huong==0) A[0].x = A[0].x + 1;
-        if (huong==1) huongA[0].y = A[0].y + 1;
-        if (huong==2) A[0].x = A[0].x - 1;
-        if (huong==3) huongA[0].y = A[0].y - 1;
-        if ((A[0].x == qua.x) && (A[0].y == qua.y)){
+        if (Huong==0) A[0].x = A[0].x + 1;
+        if (Huong==1) A[0].y = A[0].y + 1;
+        if (Huong==2) A[0].x = A[0].x - 1;
+        if (Huong==3) A[0].y = A[0].y - 1;
+        if ((A[0].x == Qua.x) && (A[0].y == Qua.y)){
             DoDai++;
-            qua.x = rand()%(MAXX-MINX)+MINX;
-            qua.y = rand()%(MAXY-MINY)+MINY;
-    }
-
-    void VeKhung() {
-        for (int i = MINX ; i<=MAXX ; i++)
-            for (int j = MINX ; j<=MAXY ; j++)
-                if ((i==MINX) || (i==MAXX) || (j==MINY) || (j==MAXY)){
-                gotoxy(i,j);
-                printf("+");
+            Qua.x = rand()%(MAXX-MINX)+MINX;
+            Qua.y = rand()%(MAXY-MINY)+MINY;
         }
     }
+};
+void VeKhung(){
+    for (int i = MINX ; i<=MAXX ; i++)
+        for (int j = MINX ; j<=MAXY ; j++)
+            if ((i==MINX) || (i==MAXX) || (j==MINY) || (j==MAXY)){
+            gotoxy(i,j);
+            printf("+");
+        }
 }
-
-int main() {
+int main()
+{
     CONRAN r;
-    int huong = 0;
+    int Huong = 0;
     char t;
-    Point qua;
+    Point Qua;
     srand((int)time(0));
-    qua.x = rand() % (MAXX - MINX) + MINX;
-    qua.y = rand() % (MAXY - MINY) + MINY;
-    while (1)
-    {
-        if(kbhit) {
-            t =getch();
-            if(t == 'a') huong = 2;
-            if(t == 'w') huong = 3;
-            if(t == 'd') huong = 0;
-            if(t == 's') huong = 1;
+    Qua.x = rand()%(MAXX-MINX)+MINX;
+    Qua.y = rand()%(MAXY-MINY)+MINY;
+    while (1){
+        if (kbhit()){
+            t = getch();
+            if (t=='a') Huong = 2;
+            if (t=='w') Huong = 3;
+            if (t=='d') Huong = 0;
+            if (t=='s') Huong = 1;
         }
         system("cls");
         VeKhung();
-        r.Ve(qua);
-        r.DiChuyen(huong, qua);
+        r.Ve(Qua);
+        r.DiChuyen(Huong, Qua);
         Sleep(300);
     }
-    
+
     return 0;
 }
 
-void gotoxy(int column, int line) {
-    COORD coord;
-    coord.X = column;
-    coord.Y = line;
-    SetConsoleCursorPosition(
-        GetStdHandle(STD_OUTPUT_HANDLE),
-        coord
+
+void gotoxy( int column, int line )
+  {
+  COORD coord;
+  coord.X = column;
+  coord.Y = line;
+  SetConsoleCursorPosition(
+    GetStdHandle( STD_OUTPUT_HANDLE ),
+    coord
     );
-}
+  }
