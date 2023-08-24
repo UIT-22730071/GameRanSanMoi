303934c4eafe1ede9ac2267db0f89ae9bf9b1ac9
UIT-22730100 <142426097+UIT-22730100@users.noreply.github.com>
2023-08-24 20:27:58 +0700

    Thêm chức năng vẽ khung

--- main.cpp
+++ main.cpp
@@ -36,7 +36,12 @@ public:
     }
 
     void VeKhung() {
-
+        for (int i = MINX ; i<=MAXX ; i++)
+            for (int j = MINX ; j<=MAXY ; j++)
+                if ((i==MINX) || (i==MAXX) || (j==MINY) || (j==MAXY)){
+                gotoxy(i,j);
+                printf("+");
+        }
     }
 }
 
